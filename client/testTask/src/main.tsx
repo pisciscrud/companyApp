0fdd7e7761384055e96f11786e3cc0790226f3cd
pisciscrud <bankviki@icloud.com>
2023-11-06 15:29:48 +0300

    fix import reacr-query

--- client/testTask/src/main.tsx
+++ client/testTask/src/main.tsx
@@ -1,7 +1,8 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
- import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
-import { trpc, trpcClient } from './utils/trpcClient';
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+import { trpc, trpcClient } from "./utils/trpcClient";
 
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")!).render(
